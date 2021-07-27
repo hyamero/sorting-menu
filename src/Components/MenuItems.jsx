ece8041470b53123eaabd4d7bbf7436db578d4f1
Dale B <daleban.dev@gmail.com>
2021-07-27 11:13:51 +0800

    Apply animations on sections.

--- src/Components/MenuItems.jsx
+++ src/Components/MenuItems.jsx
@@ -96,55 +96,130 @@ const MenuItems = ({ items, all, breakfast, lunch, shakes }) => {
         items
           .filter((item) => item.category === "breakfast")
           .map((item) => (
-            <div className="menu-items" key={item.id}>
+            <motion.div
+              className="menu-items"
+              key={item.id}
+              initial="hidden"
+              animate="visible"
+              variants={list}
+              transition={{ duration: 0.8 }}
+            >
               <img
                 src="https://miro.medium.com/max/1400/1*nKSANcE0nTsmnFY3w4O1dQ.png"
                 alt="food burger"
               />
-              <div className="item-content">
-                <div className="item-title-box">
-                  <h5 className="item-title">{item.title}</h5>
-                  <h5 className="item-price">{item.price}</h5>
-                </div>
-                <p className="item-desc">{item.desc}</p>
-              </div>
-            </div>
+              <motion.div className="item-content">
+                <motion.div className="item-title-box">
+                  <motion.h5
+                    className="item-title"
+                    variants={itemList}
+                    transition={{ delay: 0.6, duration: 0.5 }}
+                  >
+                    {item.title}
+                  </motion.h5>
+                  <motion.h5
+                    className="item-price"
+                    variants={itemList}
+                    transition={{ delay: 0.8, duration: 0.5 }}
+                  >
+                    {item.price}
+                  </motion.h5>
+                </motion.div>
+                <motion.p
+                  className="item-desc"
+                  variants={itemList}
+                  transition={{ delay: 1.2, duration: 0.5 }}
+                >
+                  {item.desc}
+                </motion.p>
+              </motion.div>
+            </motion.div>
           ))}
       {lunch &&
         items
           .filter((item) => item.category === "lunch")
           .map((item) => (
-            <div className="menu-items" key={item.id}>
+            <motion.div
+              className="menu-items"
+              key={item.id}
+              initial="hidden"
+              animate="visible"
+              variants={list}
+              transition={{ duration: 0.8 }}
+            >
               <img
                 src="https://miro.medium.com/max/1400/1*nKSANcE0nTsmnFY3w4O1dQ.png"
                 alt="food burger"
               />
-              <div className="item-content">
-                <div className="item-title-box">
-                  <h5 className="item-title">{item.title}</h5>
-                  <h5 className="item-price">{item.price}</h5>
-                </div>
-                <p className="item-desc">{item.desc}</p>
-              </div>
-            </div>
+              <motion.div className="item-content">
+                <motion.div className="item-title-box">
+                  <motion.h5
+                    className="item-title"
+                    variants={itemList}
+                    transition={{ delay: 0.6, duration: 0.5 }}
+                  >
+                    {item.title}
+                  </motion.h5>
+                  <motion.h5
+                    className="item-price"
+                    variants={itemList}
+                    transition={{ delay: 0.8, duration: 0.5 }}
+                  >
+                    {item.price}
+                  </motion.h5>
+                </motion.div>
+                <motion.p
+                  className="item-desc"
+                  variants={itemList}
+                  transition={{ delay: 1.2, duration: 0.5 }}
+                >
+                  {item.desc}
+                </motion.p>
+              </motion.div>
+            </motion.div>
           ))}
       {shakes &&
         items
           .filter((item) => item.category === "shakes")
           .map((item) => (
-            <div className="menu-items" key={item.id}>
+            <motion.div
+              className="menu-items"
+              key={item.id}
+              initial="hidden"
+              animate="visible"
+              variants={list}
+              transition={{ duration: 0.8 }}
+            >
               <img
                 src="https://miro.medium.com/max/1400/1*nKSANcE0nTsmnFY3w4O1dQ.png"
                 alt="food burger"
               />
-              <div className="item-content">
-                <div className="item-title-box">
-                  <h5 className="item-title">{item.title}</h5>
-                  <h5 className="item-price">{item.price}</h5>
-                </div>
-                <p className="item-desc">{item.desc}</p>
-              </div>
-            </div>
+              <motion.div className="item-content">
+                <motion.div className="item-title-box">
+                  <motion.h5
+                    className="item-title"
+                    variants={itemList}
+                    transition={{ delay: 0.6, duration: 0.5 }}
+                  >
+                    {item.title}
+                  </motion.h5>
+                  <motion.h5
+                    className="item-price"
+                    variants={itemList}
+                    transition={{ delay: 0.8, duration: 0.5 }}
+                  >
+                    {item.price}
+                  </motion.h5>
+                </motion.div>
+                <motion.p
+                  className="item-desc"
+                  variants={itemList}
+                  transition={{ delay: 1.2, duration: 0.5 }}
+                >
+                  {item.desc}
+                </motion.p>
+              </motion.div>
+            </motion.div>
           ))}
     </div>
   );
